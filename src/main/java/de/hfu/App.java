0fa6232681578387ac91b408b6fa0428c3a1f08f
barakatghufran <ghufran.barakat@hs-furtwangen.de>
2022-11-24 15:08:58 +0100

    änderungen

--- src/main/java/de/hfu/App.java
+++ src/main/java/de/hfu/App.java
@@ -1,9 +1,19 @@
 package de.hfu;
-
+import java.util.Scanner;
+/**
+ * @author Ghufran
+ * Zuerst ein text eingeben
+ * Zweitens Scanner hinlegen
+ * Drittens Text schrieben
+ * gegebene Text wir groß geschreiben
+ *
+ */
 public class App 
 {
     public static void main( String[] args ){
-    	 String str = "welcome to programmer world";
+    	System.out.println("Text eingeben: ");
+    	 Scanner scan = new Scanner(System.in);
+    	 String str = scan.nextLine();
          String result = str.toUpperCase();
          System.out.println("Original String  : " + str);
          System.out.println("Uppercase String : " + result);
