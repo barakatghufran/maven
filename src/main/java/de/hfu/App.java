2d6f92673c0dc9046f7863d045a2ba7817b33163
barakatghufran <ghufran.barakat@hs-furtwangen.de>
2022-11-24 14:27:22 +0100

    hinzugefuegt

---
+++ src/main/java/de/hfu/App.java
@@ -0,0 +1,12 @@
+package de.hfu;
+
+public class App 
+{
+    public static void main( String[] args ){
+    	 String str = "welcome to programmer world";
+         String result = str.toUpperCase();
+         System.out.println("Original String  : " + str);
+         System.out.println("Uppercase String : " + result);
+     
+    }
+}
